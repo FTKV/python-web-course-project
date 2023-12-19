a4413943aa563ebce149345ee69bd4f309eb9fff
FTKV <mthkvv@gmail.com>
2023-12-19 02:28:22 +0000

    tests refactoring

--- tests/test_unit_repository_users.py
+++ tests/test_unit_repository_users.py
@@ -1,18 +1,19 @@
 import pickle
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, AsyncMock
 
 from sqlalchemy.engine.result import ChunkedIteratorResult
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models import User
-from src.schemas.users import UserModel
+from src.schemas.users import UserModel, UserUpdateModel
 from src.repository.users import (
     get_user_by_email_from_cache,
     get_user_by_email,
     create_user,
     update_user,
 )
+from src.services.cloudinary import cloudinary_service
 
 
 class MockRedis:
@@ -62,13 +63,26 @@ class TestUsers(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(result.password, body.password)
         self.assertTrue(hasattr(result, "id"))
 
-    async def test_update_avatar(self):
+    async def test_update_user(self):
+        body = UserUpdateModel(
+            first_name="test",
+            last_name="test",
+            phone="1234567890",
+            birthday="2001-01-01",
+            avatar=MagicMock(),
+        )
         self.session.execute.return_value = MagicMock(spec=ChunkedIteratorResult)
         self.session.execute.return_value.scalar.return_value = self.user
         self.redis_db.expire.return_value = None
-        url = "http://test.com/avatar"
-        result = await update_avatar(self.user.email, url, self.session, self.redis_db)
-        self.assertEqual(result.avatar, url)
+        avatar_url = "http://test.com/avatar"
+        cloudinary_service.upload_avatar = AsyncMock()
+        cloudinary_service.upload_avatar.return_value = avatar_url
+        result = await update_user(self.user.email, body, self.session, self.redis_db)
+        self.assertEqual(result.first_name, body.first_name)
+        self.assertEqual(result.last_name, body.last_name)
+        self.assertEqual(result.phone, body.phone)
+        self.assertEqual(result.birthday, body.birthday)
+        self.assertEqual(result.avatar, avatar_url)
 
 
 if __name__ == "__main__":
